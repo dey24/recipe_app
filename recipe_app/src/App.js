aaf74ad782dc816b55c2d62f52382626786c6238
Chiranjit Dey <chiranjitdey2000@gmail.com>
2020-12-21 19:16:30 +0530

    rendering recipe.js

--- recipe_app/src/App.js
+++ recipe_app/src/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import "./App.css"
 
 const App = () => {
@@ -6,7 +7,8 @@ const App = () => {
   const APP_ID = '11d4be7e';
   const APP_KEY = '964916610369cf5ccdf7165f39eef5d0';
   
-
+  const [recipes, setRecipes] = useState([]);
+  
   useEffect(() =>{
     getRecipes();
   }, [])
@@ -14,7 +16,8 @@ const App = () => {
   const getRecipes = async () =>{
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
+    console.log(data.hits)
   }
 
 
@@ -24,6 +27,9 @@ const App = () => {
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe =>(
+        <Recipe />
+      ))}
     </div>
   )
 }
---
+++ recipe_app/src/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Recipe = () => {
+    return(
+        <div>
+            <h1>Title</h1>
+            <h3>Calories</h3>
+            <img src = "" alt = "" />
+        </div>
+    )
+}
+
+export default Recipe;
