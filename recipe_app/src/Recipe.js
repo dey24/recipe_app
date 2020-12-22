20e9ca935bff425ad08368d98f80088c68ded107
Chiranjit Dey <chiranjitdey2000@gmail.com>
2020-12-22 20:37:48 +0530

    heading improved

--- recipe_app/src/Recipe.js
+++ recipe_app/src/Recipe.js
@@ -4,6 +4,7 @@ import style from './recipe.module.css';
 const Recipe = ({title, calories, image, ingredients}) => {
     return(
         <div className = {style.recipe}>
+        <h1>Hello Chef!</h1>
             <h1>{title}</h1>
             <h3>{calories}</h3>
             <ol>
